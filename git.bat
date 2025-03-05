6b156f0bd82d3744fa468dfbbe740f6d78ba1a30
Tomek Małecki <maleckit@icloud.com>
2025-03-05 19:53:58 +0100

    first

---
+++ git.bat
@@ -0,0 +1,7 @@
+echo "# st" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/TomekMalecki/st.git
+git push -u origin main
